fddafd578d2ee7375f49fffb82c6ea4a26e6a15a
Ekaterina <zaloginakate@gmail.com>
2022-06-24 09:05:02 +0300

    download python dnd-character

---
+++ python/dnd-character/dnd_character.py
@@ -0,0 +1,17 @@
+import random
+import math
+
+class Character:
+    def __init__(self):
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+    def ability(self):
+        return random.randint(3,18)
+def modifier(score):
+    return math.floor((score - 10)/2)
+
