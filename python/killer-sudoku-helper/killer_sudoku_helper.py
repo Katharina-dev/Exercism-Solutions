85ab21c63a90475aa5902a675180d9befcceed06
Ekaterina <zaloginakate@gmail.com>
2023-12-19 11:36:39 +0300

    download python killer-sudoku-helper

---
+++ python/killer-sudoku-helper/killer_sudoku_helper.py
@@ -0,0 +1,11 @@
+import itertools
+def combinations(target, size, exclude):
+    arrays = [list(map(int, array)) for array in itertools.combinations('123456789', size)]
+    combs = []
+    for array in arrays:
+        for item in array:
+            if item in exclude or sum(array) != target:
+                break
+        else:
+            combs.append(array)
+    return combs
