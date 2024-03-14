9c218314faef045563342ff6fde3af45f86e3a74
Ekaterina <zaloginakate@gmail.com>
2024-03-14 12:23:23 +0300

    download python alphametics

---
+++ python/alphametics/alphametics.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+
+def solve(puzzle):
+    
+    words = puzzle.replace('+', '').replace('=', '').split()
+    first_letters = {ord(w[0]) for w in words}
+    letters = "".join(set(''.join(words)))
+    num_letters = len(letters)
+    
+    for perm in permutations(range(10), num_letters):
+        trans = str.maketrans(letters, "".join(map(str, perm)))
+        if any(trans[l] == 48 for l in first_letters):
+            continue
+        num = [word.translate(trans) for word in words]
+        *adds, target = map(int, num)
+        if sum(adds) == target:
+            return dict(zip(letters, perm))
