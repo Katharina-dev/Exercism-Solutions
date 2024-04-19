4bd6bf70a3369a8f10cc7c550e050e95b89b1a3f
Ekaterina <zaloginakate@gmail.com>
2024-04-19 11:48:42 +0300

    download python satellite

---
+++ python/satellite/satellite.py
@@ -0,0 +1,25 @@
+def tree_from_traversals(preorder, inorder):
+    
+    if len(preorder) != len(inorder):
+        raise ValueError("traversals must have the same length")
+    
+    elif set(preorder) != set(inorder):
+        raise ValueError("traversals must have the same elements")
+    
+    elif len(preorder) != len(set(preorder)) or len(inorder) != len(set(inorder)
+                                                                    ):
+        raise ValueError("traversals must contain unique items")
+    
+    return tree(preorder, inorder)
+
+        
+def tree(preorder, inorder):
+    
+    if preorder == []:
+        return {}
+    
+    root_index = inorder.index(preorder[0])
+    
+    return {"v": preorder[0],
+            "l": tree(preorder[1:root_index+1], inorder[:root_index]),
+            "r": tree(preorder[root_index+1:], inorder[root_index+1:])}
