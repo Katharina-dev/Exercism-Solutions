c017ae7021325363de03aa5375c9b42693e3f5c6
Ekaterina <zaloginakate@gmail.com>
2025-01-15 09:50:42 +0300

    download python atbash-cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,12 +1,16 @@
 def encode(plain_text):
+    
     text_array = [i for i in plain_text if i.isalnum()]
     text_cipher = [chr(26 - (ord(i.lower()) - 96) + 97) if i.isalpha() else i for i in text_array]
     parts = len(text_cipher)//5
+    
     return ' '.join([''.join(text_cipher[i*5:i*5+5]) for i in range(parts) if parts>0]  + [''.join(text_cipher[parts*5:])]).strip()
 
 
 def decode(ciphered_text):
+    
     text_array = [i for i in (ciphered_text.replace(' ', '')) if i.isalnum()]
     text_cipher = [chr(26 - (ord(i) - 96) + 97) if i.isalpha() else i for i in text_array]
+    
     return ''.join(text_cipher)
 
