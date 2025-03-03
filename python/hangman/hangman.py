633d1351b4fcec0486a32788f884b3a3426425ab
Ekaterina <zaloginakate@gmail.com>
2025-03-03 10:01:41 +0300

    download python hangman

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -1,14 +1,18 @@
 # Game status categories
 # Change the values as you see fit
+
 STATUS_WIN = 'win'
 STATUS_LOSE = 'lose'
 STATUS_ONGOING = 'ongoing'
+
 class Hangman:
+    
     def __init__(self, word):
         self.word = word
         self.mask = list('_' * len(self.word))
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        
     def guess(self, char):
         if self.status != STATUS_ONGOING:
             raise ValueError("The game has already ended.")
@@ -22,6 +26,7 @@ class Hangman:
         
     def get_masked_word(self):
         return ''.join(self.mask)
+    
     def get_status(self):
         if self.remaining_guesses < 0:
             self.status = STATUS_LOSE
