528d3563a17530d809006148ac40b877ae435c48
Ekaterina <zaloginakate@gmail.com>
2025-03-27 10:08:12 +0300

    download python queen-attack

---
+++ python/queen-attack/queen_attack.py
@@ -0,0 +1,26 @@
+class Queen:
+    
+    def __init__(self, row, column):
+        
+        if row < 0:
+            raise ValueError("row not positive")
+        elif row > 7:
+            raise ValueError("row not on board")
+        elif column < 0:
+            raise ValueError("column not positive")
+        elif column > 7:
+            raise ValueError("column not on board")
+        else:
+            self.row = row
+            self.column = column
+        
+
+    def can_attack(self, another_queen):
+        
+        if self.row == another_queen.row and self.column == another_queen.column:
+            raise ValueError("Invalid queen position: both queens in the same square")
+        elif self.row == another_queen.row or self.column == another_queen.column or abs(another_queen.row - self.row) == abs(another_queen.column - self.column):
+            return True
+        else:
+            return False
+
