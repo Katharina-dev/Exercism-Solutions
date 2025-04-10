1a16b33ecabcab4096fe6ab2c8baa2b8eb0472b1
Ekaterina <zaloginakate@gmail.com>
2025-04-10 11:35:00 +0300

    download python all-your-base

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,24 +1,31 @@
 import math
 
+
 def to_decimal(input_base, digits):
+    
     result = []
-    for pow, digit in enumerate(digits[::-1]):
-        result.append(digit * input_base ** pow)
-    return sum(result)
+    for p, digit in enumerate(digits[::-1]):
+        result.append(digit * math.pow(input_base, p))
+    return int(sum(result))
+
 
 def from_decimal(digit, output_base):
+    
     if not digit:
         return [0]
     result = []
-    pow = round(math.log(digit, output_base))
-    while pow >= 0:
-        new_digit = digit//output_base**pow
+    p = round(math.log(digit, output_base))
+    while p >= 0:
+        math_pow = math.pow(output_base, p)
+        new_digit = digit//math_pow
         result.append(new_digit)
-        digit -= new_digit*output_base**pow
-        pow -= 1
+        digit -= new_digit*math_pow
+        p -= 1
     return result
 
+
 def rebase(input_base, digits, output_base):
+    
     if input_base < 2:
         raise ValueError("input base must be >= 2")
     elif [digit for digit in digits if digit < 0 or digit >= input_base]:
@@ -29,4 +36,4 @@ def rebase(input_base, digits, output_base):
         return [int(i) for i in list(str(to_decimal(input_base, digits)))]
     elif input_base == 10:
         return from_decimal(int(''.join([str(i) for i in digits])), output_base)
-    return from_decimal(to_decimal(input_base, digits), output_base)
+    return from_decimal(to_decimal(input_base, digits), output_base) 
