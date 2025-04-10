b4dcc1a49fdbedf0246ecc53c00a6855b3705320
Ekaterina <zaloginakate@gmail.com>
2025-04-10 11:09:03 +0300

    download python pig-latin

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,14 +1,14 @@
 import re
+
 def translate(text):
+    
     return " ".join([trans_word(word) for word in text.split()])
     
 def trans_word(word):
-    trans = re.match(r'([euioa]|xr|yt)(\w+)|((?:[^euioaxyq](?!qu)|x(?!r)|y(?!t))[^euioaxyq]*)(\w+)|([^euioaxy]?qu)(\w+)|(q)([^u]+)', word)
+    
+    trans = re.match(r'([euioa]|xr|yt)(\w+)|((?:[^euioaq](?!qu))[^euioay]*)(\w+)|([^euioa]?qu)(\w+)|(q)([^u]+)', word)
     for i in range(1,9,2):
-        if trans.group(1) and i == 1:
+        if trans.group(1):
             return f'{trans.group(1)}{trans.group(2)}ay'
         elif trans.group(i):
             return f'{trans.group(i+1)}{trans.group(i)}ay'
-
-
-
