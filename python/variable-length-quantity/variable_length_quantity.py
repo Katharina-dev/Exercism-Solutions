9800d242f81bfc157a8b7a7c1f08051163b2a712
Ekaterina <zaloginakate@gmail.com>
2025-04-25 09:16:08 +0300

    download python variable-length-quantity

--- python/variable-length-quantity/variable_length_quantity.py
+++ python/variable-length-quantity/variable_length_quantity.py
@@ -1,26 +1,23 @@
+vlq_num_mask = 0b1111111
+vlq_continue_mask = 128
+
 def encode(numbers):
-    result = []
-    for number in numbers:
-        bin_number = bin(number)[2:]
-        len_number = (len(bin_number)//7+1)*7 if len(bin_number)%7 else len(bin_number)
-        bin_number = bin_number.zfill(len_number)
-        vlq = []
-        for i in range(0,len(bin_number),7):
-            vlq.append('1'+bin_number[i:i+7])
-        vlq[-1] = '0' + vlq[-1][1:]
-        result.extend(vlq)
-    return [int(num, 2) for num in result]
+    
+    return [((number>>(7*i)) & vlq_num_mask) | (bool(i)<<7)
+            for number in numbers
+            for i in range(((number>>1).bit_length())//7, -1, -1)]
 
 
 def decode(bytes_):
-    if bin(bytes_[-1])[2:].zfill(8)[0] == '1':
-        raise ValueError("incomplete sequence")
-    numbers = []
-    number = []
+    
+    result = []
+    code = 0
+    if bytes_[-1] & vlq_continue_mask:
+        raise ValueError('incomplete sequence')
     for byte in bytes_:
-        msb = bin(byte)[2:].zfill(8)[0]
-        number.append(bin(byte)[2:].zfill(8)[1:])
-        if msb == '0':
-            numbers.append(''.join(number))
-            number = []
-    return [int(number,2) for number in numbers]
+        code = (code << 7)|(byte & vlq_num_mask)
+        if not(byte & vlq_continue_mask):
+            result.append(code)
+            code = 0
+            
+    return result
