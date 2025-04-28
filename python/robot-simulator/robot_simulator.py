9e81bc543b408dbcbcf2e0133d65b47e0eb237c9
Ekaterina <zaloginakate@gmail.com>
2025-04-28 10:08:54 +0300

    download python robot-simulator

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -6,16 +6,20 @@ WEST = 3
 SOUTH = 2
 
 class Robot:
+    
     def __init__(self, direction=NORTH, x_pos=0, y_pos=0):
         self.direction = direction
         self.x_pos = x_pos
         self.y_pos = y_pos
+
         
     @property
     def coordinates(self):
         return (self.x_pos, self.y_pos)
+
     
     def move(self, movement):
+        
         for m in movement:
             if m == "R":
                 self.direction = (self.direction + 1)%4
