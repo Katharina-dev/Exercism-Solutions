fe46d7b6200b7d6415997b94f0e38c254813202d
Ekaterina <zaloginakate@gmail.com>
2025-04-28 10:06:19 +0300

    download python diamond

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -1,8 +1,10 @@
 def rows(letter):
+    
     number = ord(letter) - ord("A") + 1
     result_quarter = [[chr(j + ord("A")) if j == i else " " for j in range(number)] for i in range(number)]
     result_half = [result_quarter[i][-1:0:-1]+result_quarter[i] for i in range(number)]
     result = result_half + result_half[::-1][1:]
+    
     return ["".join(i) for i in result]
 
 
