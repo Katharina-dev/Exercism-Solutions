99616ff45eacd4a512ad4f14c4718422ac107cec
Ekaterina <zaloginakate@gmail.com>
2025-06-05 10:19:44 +0300

    download python high-scores

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,16 @@
+class HighScores():
+
+    def __init__(self, scores):
+        self.scores = scores
+        self.sorted_scores = sorted(self.scores, reverse=True)
+
+    def latest(self):
+        return self.scores[-1]
+
+
+    def personal_best(self):
+        return self.sorted_scores[0]
+
+
+    def personal_top_three(self):
+        return self.sorted_scores[:3]
