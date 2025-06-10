c01f4f10b32978dc5ec5852a22c124fac743795d
Ekaterina <zaloginakate@gmail.com>
2025-06-10 09:44:49 +0300

    download python nth-prime

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,15 +1,16 @@
 def prime(number):
+    
     if number < 1:
         raise ValueError('there is no zeroth prime')
+    
     prime_numbers = [2]
     n = 2
+    
     while len(prime_numbers) != number:
         n += 1
-        for i in prime_numbers:
-            if n%i == 0:
-                break
-        else:
+        if all(n % i > 0 for i in prime_numbers):
             prime_numbers.append(n)
+            
     return prime_numbers[-1]
 
 
