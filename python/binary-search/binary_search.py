9c51b4db11581f69fc4e93a5f8e176cdf1efa30d
Ekaterina <zaloginakate@gmail.com>
2025-06-22 13:39:07 +0300

    download python binary-search

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,13 +1,19 @@
 def find(search_list, value):
+    
     start = 0
-    end = len(search_list)
-    while end - start > 1:
+    end = len(search_list) - 1
+    
+    while end - start > 0:
         partition = (end-start)//2 + start
-        if value in search_list[start:partition]:
-            end = partition
-        else:
-            start = partition
-    if not search_list or search_list[start] != value:
+        if search_list[partition] == value:
+            return partition
+        elif value < search_list[partition]:
+            end = partition - 1
+        elif value > search_list[partition]:
+            start = partition + 1
+            
+    if search_list and search_list[start] == value:
+        return start 
+    else:
         raise ValueError("value not in array")
-    return start
 
