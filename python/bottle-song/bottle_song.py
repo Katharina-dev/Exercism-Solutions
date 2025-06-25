5621615c4f2d88b43c4ef741aae3c80dfa1ba3e3
Ekaterina <zaloginakate@gmail.com>
2025-06-25 09:18:32 +0300

    download python bottle-song

--- python/bottle-song/bottle_song.py
+++ python/bottle-song/bottle_song.py
@@ -1,11 +1,14 @@
 def recite(start, take=1):
+    
     numbers = ['no', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
     endings = {True: 's', False: ''}
     song = []
+    
     for i in range(start, start-take, -1):
         song.append("")
         song.extend(2 * [f"{numbers[i].capitalize()} green bottle{endings[bool(i-1)]} hanging on the wall,"])
         song.append("And if one green bottle should accidentally fall,")
         song.append(f"There'll be {numbers[i-1]} green bottle{endings[bool(i-2)]} hanging on the wall.")
+        
     return song[1:]
 
