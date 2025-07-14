11007dc511ed65442431164d426fb981745d4190
Ekaterina <zaloginakate@gmail.com>
2025-07-14 15:46:29 +0300

    download python flatten-array

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,5 +1,7 @@
 def flatten(iterable):
+    
     result = [i.strip() for i in str(iterable).replace("[","").replace("]","").split(",")]
-    return [int(i) for i in result if i != "None" and i != ""]
+    
+    return [int(i) for i in result if i not in ("None", "")]
 
 
