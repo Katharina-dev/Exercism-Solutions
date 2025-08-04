2c1a0e040156ca550124a14e4bf26c84a54308a1
Ekaterina <zaloginakate@gmail.com>
2025-08-04 09:31:54 +0300

    download python list-ops

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,17 +1,14 @@
 def append(list1, list2):
     return list1 + list2
 
-
 def concat(lists):
     result = []
     for i in lists:
         result += i
     return result
 
-
 def filter(function, list):
     return [x for x in list if function(x)]
-
 
 def length(list):
     count = 0
@@ -19,22 +16,18 @@ def length(list):
         count += 1
     return count
 
-
 def map(function, list):
     return [function(x) for x in list]
-
 
 def foldl(function, list, initial):
     for i in list:
         initial = function(initial,i)
     return initial
 
-
 def foldr(function, list, initial):
     for i in list[::-1]:
         initial = function(initial,i)
-    return initial if type(initial) == int else initial[::-1]
-
+    return initial
 
 def reverse(list):
     return list[::-1]
