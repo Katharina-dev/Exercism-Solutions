136ee11557122e2778eb296ba096877d273a1435
Ekaterina <zaloginakate@gmail.com>
2025-08-11 09:21:02 +0300

    download python etl

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,8 +1,11 @@
 def transform(legacy_data):
+    
     result = {}
+    
     for i in legacy_data:
         for j in legacy_data[i]:
             result[j.lower()] = i
+            
     return dict(sorted(result.items()))
 
 
