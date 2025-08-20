9e71f5b91b0e9d115ea4e6286f34ca0d78ba3c9d
Ekaterina <zaloginakate@gmail.com>
2025-08-20 11:10:53 +0300

    download python linked-list

--- python/linked-list/linked_list.py
+++ python/linked-list/linked_list.py
@@ -1,47 +1,102 @@
 class Node:
-    def __init__(self, value, succeeding=None, previous=None):
+    
+    def __init__(self, value, prev=None, foll=None):
         self.value = value
-        self.succeeding = succeeding
-        self.previous = previous
-
-
+        self.prev = prev
+        self.foll = foll
+        
 class LinkedList:
+    
     def __init__(self):
-        self.tail = None
         self.head = None
+        self.tail = None
+        self.length = 0
+        
+    def __len__(self):
+        return self.length
+        
+    def __iter__(self):
+        current_node = self.head
+        while current_node:
+            yield current_node.value
+            current_node = current_node.foll
+            
     def push(self, value):
         new_node = Node(value)
-        if self.tail:
-            self.tail.previous = new_node
-            new_node.succeeding = self.tail
-        else:
+        
+        if self.length == 0:
             self.head = new_node
-        self.tail = new_node
+            self.tail = new_node
+        else:
+            self.tail.foll = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        
+        self.length += 1
+        
     def pop(self):
-        self.del_node = self.tail
-        if self.tail.succeeding:
-            self.tail = self.tail.succeeding
-            self.tail.previous = None
-        else:
+        if self.length == 0:
+            raise IndexError("List is empty")
+        
+        popped_value = self.tail.value
+        
+        if self.length == 1:
             self.head = None
             self.tail = None
-        return self.del_node.value
-
+        else:
+            self.tail = self.tail.prev
+            self.tail.foll = None
+            
+        self.length -= 1
+        return popped_value
+        
     def shift(self):
-        self.del_node = self.head
-        if self.head.previous:
-            self.head = self.head.previous
-            self.head.previous = None
-        else:
+        if self.length == 0:
+            raise IndexError("List is empty")
+        
+        shifted_value = self.head.value
+        
+        if self.length == 1:
             self.head = None
             self.tail = None
-        return self.del_node.value
-
+        else:
+            self.head = self.head.foll
+            self.head.prev = None
+            
+        self.length -= 1
+        return shifted_value
+        
     def unshift(self, value):
         new_node = Node(value)
-        if self.head:
-            self.head.succeeding = new_node
-            new_node.previous = self.head
-        else:
+        if self.length == 0:
+            self.head = new_node
             self.tail = new_node
-        self.head = new_node
+        else:
+            new_node.foll = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            
+        self.length += 1
+        
+    def delete(self, value):
+        if self.length == 0:
+            raise ValueError("Value not found")
+        current_node = self.head
+       
+        while current_node:
+            if current_node.value == value:
+                if current_node.prev:
+                    current_node.prev.foll = current_node.foll
+                else:
+                    self.head = current_node.foll
+                
+                if current_node.foll:
+                    current_node.foll.prev = current_node.prev
+                else:
+                    self.tail = current_node.prev
+                
+                self.length -= 1
+                return 
+            current_node = current_node.foll
+        
+        raise ValueError("Value not found")
